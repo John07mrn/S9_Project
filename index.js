ccc61a00c5f5f012f6452126b089cf40d335874e
John07mrn <john1992miron@gmail.com>
2024-10-06 13:04:54 +0100

    add Variables

---
+++ index.js
@@ -0,0 +1,52 @@
+// Declare a variable for your age and display it in the console.
+let age = 32;
+console.log(age);
+
+// Calculate the sum of two numbers and display the result.
+let num1 = 8;
+let num2 = 4;
+let sum = num1 + num2;
+console.log(sum);
+
+// Declare a variable for the temperature in Celsius and convert it to Fahrenheit.
+let celsiusTemperature = 25;
+let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+console.log(fahrenheitTemperature);
+
+// Assign a string with the value "Hello, " and add your name, then display the result.
+let greeting = "Hello, ";
+let myName = "John!";
+let fullGreeting = greeting + myName;
+console.log(fullGreeting);
+
+// Check if a number is even or odd and display the result.
+let numberToCheck = 9;
+let isEven = numberToCheck % 2 === 0;
+console.log("The number is even: ", isEven);
+
+// Declare a boolean variable to check if you are a student or not.
+let isStudent = true;
+console.log(isStudent);
+
+// Declare two string variables and concatenate them.
+let firstName = "John";
+let lastName = "Mrn";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Calculate the remainder of the division of two numbers and display the result.
+let dividend = 14;
+let divisor = 4;
+let remainder = dividend % divisor;
+console.log(remainder);
+
+// Declare a variable and assign it the value undefined, then display the result.
+let undefinedVariable;
+console.log(undefinedVariable);
+
+// Calculate the area of a rectangle using the length and width.
+let length = 20;
+let width = 8;
+let area = length * width;
+console.log(area);
+
